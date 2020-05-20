622623890a6db638592ff23582f861278db4266f
WEWE <wjw603053937@gmail.com>
2020-05-20 17:52:58 +0800

    AOP1.0

--- src/main/java/com/wewe/controller/DispatcherServlet.java
+++ src/main/java/com/wewe/controller/DispatcherServlet.java
@@ -16,11 +16,10 @@ public class DispatcherServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("request path is : " + req.getServletPath());
         System.out.println("request method is : " + req.getMethod());
-        if (req.getServletPath().equals("/frontend/getmainpageinfo") && req.getMethod() == "GET"){
+        if (req.getServletPath().equals("/frontend/getmainpageinfo") && req.getMethod() == "GET") {
             new MainPageController().getMainPageInfo(req, resp);
-        } else if(req.getServletPath().equals("/superadmin/addheadline") && req.getMethod() == "POST"){
+        } else if (req.getServletPath().equals("/superadmin/addheadline") && req.getMethod() == "POST") {
             new HeadLineOperationController().addHeadLine(req, resp);
         }
     }
-
 }
